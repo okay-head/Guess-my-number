b3592dc46c6e7f604f513998bddc45f5fb29e4e8
admin-on-win10 <shashwatjaiswal14@gmail.com>
2021-06-21 08:51:10 +0530

    added files

---
+++ script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+let x = Math.round(Math.random()*20);
+
+document.querySelector('.button .btn').addEventListener("click",getValue);
+
+let hint = document.querySelector('.right .hint');
+
+let input = document.querySelector('.left input[type="number"]');
+
+let score_element  = document.getElementById('score');
+let score = document.getElementById('score').innerHTML;
+
+let question = document.getElementById('question');
+
+//instead use functions
+
+function getValue(){
+   let z =document.getElementById('input').value;
+   if (score>1) {
+      
+      if (z==x) {
+         hint.innerHTML = 'Correct!';
+         question.innerHTML = x;
+         document.body.style.background = "green";
+         input.style.background = "green";
+         // console.log('Correct');
+      }
+      else{
+         score--;
+         score_element.innerHTML = score;
+
+         hint.innerHTML = z<x?'Too low':'Too high';
+      }
+
+      // REFACTORING IMP 
+
+      //else if (z<x){
+      //    // console.log('Too low');
+      //    score--;
+      //    score_element.innerHTML = score;
+   
+      //    hint.innerHTML = 'Too low';
+         
+      // }
+      // else if(z>x){
+      //    // console.log('Too high');
+      //    score--;
+      //    score_element.innerHTML = score;
+      //    hint.innerHTML = 'Too high';
+      // }
+   }
+
+   else{
+      score--;
+      score_element.innerHTML = score;
+     hint.innerHTML = "Guess who's the loser";
+     document.body.style.background = "red";
+     input.style.background = "red";
+   }
+}
+
+let again = document.querySelector('.flex .btn');
+
+again.addEventListener('click',Rload);
+
+function Rload() {
+   window.location.reload();
+}
+
+
+
+
